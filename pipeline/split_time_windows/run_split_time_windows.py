57ae1cfaf39070856efda0c6a71bd703d612be67
Michael Reimann <michael@reimann.science>
2020-07-29 19:06:32 +0200

    Now using the t_stim_start in the config

--- pipeline/split_time_windows/run_split_time_windows.py
+++ pipeline/split_time_windows/run_split_time_windows.py
@@ -5,18 +5,18 @@ from toposample import config
 def execute_split(spikes, stimuli, data_cfg):
     assert len(numpy.unique(stimuli)) == data_cfg["num_stimuli"]
     splt_t = data_cfg["stim_duration_ms"]
-    t_bins = numpy.arange(0, numpy.max(spikes[:, 0]) + splt_t, splt_t)
+    t_bins = numpy.arange(data_cfg["t_stim_start"], numpy.max(spikes[:, 0]) + splt_t, splt_t)
     t_bin_idx = numpy.digitize(spikes[:, 0], bins=t_bins) - 1
     assert numpy.max(t_bin_idx) < len(stimuli)
 
     list_of_lists = [[] for _ in range(data_cfg["num_stimuli"])]
     for i, stim in enumerate(stimuli):
-        curr_t_start = i * data_cfg["stim_duration_ms"]
-        curr_t_end = curr_t_start + data_cfg["stim_duration_ms"]
+        curr_t_start = i * splt_t
+        curr_t_end = curr_t_start + splt_t
         win = spikes[t_bin_idx == i]
         if len(win) == 0:
             print("Warning: no spikes between {0} and {1} ms".format(curr_t_start, curr_t_end))
-        win[:, 0] = win[:, 0] - curr_t_start
+        win[:, 0] = win[:, 0] - curr_t_start - data_cfg["t_stim_start"]
         list_of_lists[stim].append(win)
     return list_of_lists
 
