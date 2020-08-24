3e65904ae3780ba47ddbad4ea9de65a2f262baf8
henri.riihimaki <s02hr9@maxlogin1.maxwell.local>
2020-08-24 08:50:31 +0100

    Added module for normalized Betti coefficient in gen_topo_db

---
+++ pipeline/gen_topo_db/normalized_betti_coefficient.py
@@ -0,0 +1,25 @@
+import numpy as np
+import progressbar
+from toposample.indexing import GidConverter
+import pyflagser
+
+def compute(tribes, adj_matrix, precision):
+
+    # Normalized Betti coefficients
+    nbcs = []
+    pbar = progressbar.ProgressBar()
+    conv = GidConverter(tribes)
+
+    for tribe in pbar(tribes):
+
+        # Convert from GIDs to local indexing
+        tribe_local_indices = conv.indices(tribe)
+
+        adj_submat = adj_matrix[np.ix_(tribe_local_indices, tribe_local_indices)]
+        bettinumbers = pyflagser.flagser_unweighted(adj_submat, directed=True)['betti']
+        cellcounts = pyflagser.flagser_unweighted(adj_submat, directed=True)['cell_count']
+
+        parameter = sum(list(map(lambda x: (x+1)*bettinumbers[x]/cellcounts[x] if not is_zero(cellcounts[x]) else 0, range(min(len(bettinumbers),len(cellcounts))))))
+        nbcs.append(np.round(parameter,precision))
+
+    return nbcs
