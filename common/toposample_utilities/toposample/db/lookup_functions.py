0da3b5a3b07851674316458aaab8f43ea56c878c
Michael Reimann <michael@reimann.science>
2020-08-27 11:03:12 +0200

    Added required lookup functions

--- common/toposample_utilities/toposample/db/lookup_functions.py
+++ common/toposample_utilities/toposample/db/lookup_functions.py
@@ -6,3 +6,29 @@ def smallest_nonzero_value(v):
     if len(v) > 0:
         return numpy.min(v)
     return 0
+
+
+def smallest_nonzero_absolute_value(v):
+    v = numpy.abs(v)
+    return smallest_nonzero_value(v)
+
+
+def difference_between_largest_values(v):
+    if len(v) < 2:
+        return 0.0
+    v = numpy.sort(v)
+    return v[-1] - v[-2]
+
+
+def difference_between_largest_absolute_values(v):
+    v = numpy.abs(v)
+    return difference_between_largest_values(v)
+
+
+def largest_value(v):
+    return numpy.max(v)
+
+
+def largest_absolute_value(v):
+    v = numpy.abs(v)
+    return largest_value(v)
