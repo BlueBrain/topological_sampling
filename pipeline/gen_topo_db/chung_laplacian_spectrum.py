Fixed chung_laplacian_spectrum eigenvalue computation

pipeline/gen_topo_db/chung_laplacian_spectrum.py
@@ -1,6 +1,6 @@
 import numpy as np
 import progressbar
-import scipy
+import scipy.linalg
 
 def compute(tribes, adj_matrix, conv, precision):
     import networkx as nx